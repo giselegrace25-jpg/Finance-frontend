8d27dbaff036cd7182dcdacc6235568f6026000f
giselegrace25-jpg <giselegrace25@gmail.com>
2026-09-14 00:38:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000',
-    },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts:['https://finance-frontend-50cw.onrender.com'],
+  //server: {
+  //  proxy: {
+   //   '/api': 'http://localhost:4000',
+   // },
+ // },
   },
 })
